505d4cd9108a1cf092e6f0393c7c776cd68c72ea
Taras Oksencyk <taras.oksenchuk@yandex.ru>
2025-09-24 18:50:49 +0500

    created endpoins note

--- app/routers/notes.py
+++ app/routers/notes.py
@@ -2,6 +2,7 @@ from ..db import get_db
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Depends, APIRouter
 from .. import models, schemas
+from typing import List
 
 router = APIRouter(prefix="/notes", tags=["Notes"])
 
@@ -18,7 +19,34 @@ def create_note(note: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_note
 
 
-@router.get("/")
-def get_notes(db: Session = Depends(get_db)):
+@router.get("/", response_model=List[schemas.PostResponse])
+def get_all_notes(db: Session = Depends(get_db)):
     return db.query(models.Note).all()
+
+@router.get("/{note_id}", response_model=schemas.PostResponse)
+def get_note(note_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Note).filter(models.Note.id == note_id).first()
+
+@router.put("/{note_id}", response_model=schemas.PostResponse)
+def put_note(
+    note_id: int, 
+    update_note: schemas.PostUpdate, 
+    db: Session = Depends(get_db)
+):
+    note_query = db.query(models.Note).filter(models.Note.id == note_id)
+    note_query.update(update_note.model_dump(), synchronize_session='fetch')
+    db.commit()
+    update_note = note_query.first()
+    return update_note
+
+@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_note(
+    note_id: int, 
+    db: Session = Depends(get_db)
+):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    db.delete(note)
+    db.commit()
+    
+
 
--- app/schemas.py
+++ app/schemas.py
@@ -11,15 +11,20 @@ class PostCreate(PostBase):
     pass
 
 class PostResponse(PostBase):
+    id: int
     pass 
 
 class PostUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=1, max_length=200)
     content: Optional[str] = Field(None, min_length=1)
 
+    model_config = {"from_attributes": True}
+
 class UserBase(BaseModel):
     email: EmailStr
     password: str
+
+    model_config = {"from_attributes": True}
 
 class UserLogin(UserBase):
     pass
